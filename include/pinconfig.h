05e8e0973ebf5a77744780df266e59d5b6a24333
Bernhard Tittelbach <bernhard@tittelbach.org>
2017-03-14 23:20:35 +0100

    port with pulldown

--- include/pinconfig.h
+++ include/pinconfig.h
@@ -1,7 +1,8 @@
 #ifndef USERCONFIG_H
 #define USERCONFIG_H
 
-#define PIR_PIN 0 //D3
+//#define PIR_PIN 0 //D3 internal pullup
+#define PIR_PIN 4 //D2 internal pulldown
 
 const uint32_t TELNET_PORT_ = 2323;
 
